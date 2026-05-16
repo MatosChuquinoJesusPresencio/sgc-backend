23ab014846fe3d0b99fcf53f274a21df1ba6e15c
MatosChuquinoJesusPresencio <elcevichexd06>
2026-05-16 10:56:53 -0500

    Actualizando el port de condominio

--- src/main/java/com/condominios/sgc/domain/port/CondominioPort.java
+++ src/main/java/com/condominios/sgc/domain/port/CondominioPort.java
@@ -1,13 +1,14 @@
 package com.condominios.sgc.domain.port;
 
-import java.util.List;
 import java.util.Optional;
 
+import com.condominios.sgc.domain.dto.PaginacionRequest;
+import com.condominios.sgc.domain.dto.PaginacionResponse;
 import com.condominios.sgc.domain.model.CondominioModel;
 
 public interface CondominioPort {
     Optional<CondominioModel> findById(Long id);
-    List<CondominioModel> findAll();
+    PaginacionResponse<CondominioModel> buscarPaginado(PaginacionRequest request);
     CondominioModel save(CondominioModel model);
     void deleteById(Long id);
     boolean existsByNombre(String nombre);
