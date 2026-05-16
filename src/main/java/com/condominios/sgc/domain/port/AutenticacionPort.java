cb0c09055c901b8911f5678506c21764f55c9b11
MatosChuquinoJesusPresencio <elcevichexd06>
2026-05-16 15:09:15 -0500

    Agregando port para la autentiacion

---
+++ src/main/java/com/condominios/sgc/domain/port/AutenticacionPort.java
@@ -0,0 +1,20 @@
+package com.condominios.sgc.domain.port;
+
+import com.condominios.sgc.domain.auxiliar.SesionUsuario;
+
+public interface AutenticacionPort {
+
+    SesionUsuario iniciarSesion(String email, String password);
+
+    void cerrarSesion(String accessToken);
+
+    String crearUsuario(String email, String password, String rol);
+
+    void enviarRecuperacionContrasena(String email);
+
+    void restablecerContrasena(String token, String nuevaContrasena);
+
+    void cambiarContrasena(String accessToken, String nuevaContrasena);
+
+    void actualizarEmail(String token, String nuevoEmail);
+}
