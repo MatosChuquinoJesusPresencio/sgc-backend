5100bfefffcb00eaba8a67e8d482fb94da142738
AndreTs8 <andre.ts8@hotmail.com>
2026-05-18 22:10:07 -0500

    updatexd

---
+++ src/main/java/com/condominios/sgc/application/impl/ActualizarApartamentoUseCaseImpl.java
@@ -0,0 +1,26 @@
+package com.condominios.sgc.application.impl;
+
+import com.condominios.sgc.application.dto.ActualizarApartamentoRequest;
+import com.condominios.sgc.application.usecase.ActualizarApartamentoUseCase;
+import com.condominios.sgc.application.usecase.ObtenerApartamentoUseCase;
+import com.condominios.sgc.domain.model.ApartamentoModel;
+import com.condominios.sgc.domain.port.ApartamentoPort;
+
+public class ActualizarApartamentoUseCaseImpl implements ActualizarApartamentoUseCase {
+    private final ApartamentoPort apartamentoPort;
+    private final ObtenerApartamentoUseCase obtenerApartamentoUseCase;
+
+    public ActualizarApartamentoUseCaseImpl(ApartamentoPort apartamentoPort, ObtenerApartamentoUseCase obtenerApartamentoUseCase) {
+        this.apartamentoPort = apartamentoPort;
+        this.obtenerApartamentoUseCase = obtenerApartamentoUseCase;
+    }
+
+    @Override
+    public ApartamentoModel ejecutar(Long id, ActualizarApartamentoRequest request) {
+        ApartamentoModel apartamento = obtenerApartamentoUseCase.ejecutar(id);
+
+        apartamento.actualizarDatos(request.numero(), request.derechoEstacionamiento(), request.metraje(), request.propietarioId());
+
+        return apartamentoPort.save(apartamento);
+    }
+}
