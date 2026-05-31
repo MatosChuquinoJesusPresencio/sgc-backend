886194dcafc248467e4751e4d9c5dc7b5a56077c
JesusMatos <elcevichexd06@gmail.com>
2026-05-30 19:20:46 -0500

    arreflando entidades

---
+++ src/main/java/com/condominios/sgc/infrastructure/persistence/entity/CondominioEntity.java
@@ -0,0 +1,37 @@
+package com.condominios.sgc.infrastructure.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "condominio")
+@Getter
+@Setter
+@NoArgsConstructor
+public class CondominioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String pais;
+
+    @Column(nullable = false)
+    private String ciudad;
+
+    @Column(nullable = false)
+    private String direccion;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime fechaCreacion;
+
+    @OneToOne(mappedBy = "condominio", cascade = CascadeType.ALL, orphanRemoval = true)
+    private ConfiguracionEntity configuracion;
+}
