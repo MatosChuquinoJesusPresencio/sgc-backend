333a2ec57ae82821ed648181dc27bf2f91ca7df8
AndreTs8 <andre.ts8@hotmail.com>
2026-06-09 21:40:07 -0500

    creando el caso de uso del arbol de ObtenerArbolCondominioUseCaseImpl

--- src/main/java/com/condominios/sgc/application/impl/ObtenerArbolCondominioUseCaseImpl.java
+++ src/main/java/com/condominios/sgc/application/impl/ObtenerArbolCondominioUseCaseImpl.java
@@ -1,4 +1,19 @@
 package com.condominios.sgc.application.impl;
 
-public class ObtenerArbolCondominioUseCaseImpl {
+import com.condominios.sgc.application.usecase.ObtenerArbolCondominioUseCase;
+import com.condominios.sgc.domain.port.CondominioTreePort;
+import com.condominios.sgc.web.dto.CondominioTreeResponse;
+
+public class ObtenerArbolCondominioUseCaseImpl implements ObtenerArbolCondominioUseCase {
+
+    private final CondominioTreePort condominioTreePort;
+
+    public ObtenerArbolCondominioUseCaseImpl(CondominioTreePort condominioTreePort) {
+        this.condominioTreePort = condominioTreePort;
+    }
+
+    @Override
+    public CondominioTreeResponse ejecutar(Long condominioId) {
+        return condominioTreePort.obtenerArbolCompleto(condominioId);
+    }
 }
