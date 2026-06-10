efad6b448bfc287d0ce90da1792207e089a1a526
AndreTs8 <andre.ts8@hotmail.com>
2026-06-09 21:24:20 -0500

    meotod en ListarApartamentosFiltradosUseCase.java

--- src/main/java/com/condominios/sgc/application/usecase/ListarApartamentosFiltradosUseCase.java
+++ src/main/java/com/condominios/sgc/application/usecase/ListarApartamentosFiltradosUseCase.java
@@ -1,4 +1,9 @@
 package com.condominios.sgc.application.usecase;
 
+import com.condominios.sgc.domain.dto.PaginacionRequest;
+import com.condominios.sgc.domain.dto.PaginacionResponse;
+import com.condominios.sgc.domain.model.ApartamentoModel;
+
 public interface ListarApartamentosFiltradosUseCase {
+    PaginacionResponse<ApartamentoModel> ejecutar(Long condominioId, Long torreId, Long pisoId, PaginacionRequest request);
 }
