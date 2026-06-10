ac9ee1448c1ce75078fc3acc62808ab6b8913d05
AndreTs8 <andre.ts8@hotmail.com>
2026-06-09 21:26:11 -0500

    metodo en ListarApartamentosFiltradosUseCaseImpl.java

--- src/main/java/com/condominios/sgc/application/impl/ListarApartamentosFiltradosUseCaseImpl.java
+++ src/main/java/com/condominios/sgc/application/impl/ListarApartamentosFiltradosUseCaseImpl.java
@@ -1,4 +1,20 @@
 package com.condominios.sgc.application.impl;
 
-public class ListarApartamentosFiltradosUseCaseImpl {
+import com.condominios.sgc.application.usecase.ListarApartamentosFiltradosUseCase;
+import com.condominios.sgc.domain.dto.PaginacionRequest;
+import com.condominios.sgc.domain.dto.PaginacionResponse;
+import com.condominios.sgc.domain.model.ApartamentoModel;
+import com.condominios.sgc.domain.port.ApartamentoPort;
+
+public class ListarApartamentosFiltradosUseCaseImpl implements ListarApartamentosFiltradosUseCase {
+    private final ApartamentoPort apartamentoPort;
+
+    public ListarApartamentosFiltradosUseCaseImpl(ApartamentoPort apartamentoPort) {
+        this.apartamentoPort = apartamentoPort;
+    }
+
+    @Override
+    public PaginacionResponse<ApartamentoModel> ejecutar(Long condominioId, Long torreId, Long pisoId, PaginacionRequest request) {
+        return apartamentoPort.findByFiltros(condominioId, torreId, pisoId, request);
+    }
 }
