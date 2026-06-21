884e9665c36c555d5178ffb036c4bdc4065e1a47
JesusMatos <elcevichexd06@gmail.com>
2026-06-20 22:00:51 -0500

    add: agregnado servicio para obtener el id y rol del usuario actual

--- src/main/java/com/condominios/sgc/application/port/in/ObtenerUsuarioActualUseCase.java
+++ src/main/java/com/condominios/sgc/application/port/in/ObtenerUsuarioActualUseCase.java
@@ -3,5 +3,5 @@ package com.condominios.sgc.application.port.in;
 import com.condominios.sgc.application.dto.result.UsuarioActualResult;
 
 public interface ObtenerUsuarioActualUseCase {
-    UsuarioActualResult ejecutar(Long id);
+    UsuarioActualResult ejecutar();
 }
