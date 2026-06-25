954661551262bc67f9c54299cbc367112e80253a
AndreTs8 <andre.ts8@hotmail.com>
2026-06-25 12:49:50 -0500

    fix error de anotaciones fix error de anotacion en ActualizarAdministradorRequest

--- src/main/java/com/condominios/sgc/infrastructure/adapter/in/web/dto/request/ActualizarPerfilRequest.java
+++ src/main/java/com/condominios/sgc/infrastructure/adapter/in/web/dto/request/ActualizarPerfilRequest.java
@@ -1,9 +1,13 @@
 package com.condominios.sgc.infrastructure.adapter.in.web.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record ActualizarPerfilRequest(
     @NotBlank String nombres,
     @NotBlank String apellidos,
+
+    @NotBlank
+    @Size(max = 9, message = "El teléfono debe tener 9 caracteres")
     String telefono
 ) {}
