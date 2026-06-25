a863aa208ea26c67af262f691034058068aba96d
AndreTs8 <andre.ts8@hotmail.com>
2026-06-25 12:46:19 -0500

    fix error de anotacion en CrearAdminUserRequest

--- src/main/java/com/condominios/sgc/infrastructure/adapter/in/web/dto/request/CrearAdminUserRequest.java
+++ src/main/java/com/condominios/sgc/infrastructure/adapter/in/web/dto/request/CrearAdminUserRequest.java
@@ -2,12 +2,16 @@ package com.condominios.sgc.infrastructure.adapter.in.web.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record CrearAdminUserRequest(
     @NotBlank String nombres,
     @NotBlank String apellidos,
     @NotBlank @Email String correo,
-    @NotBlank String telefono,
+
+    @NotBlank
+    @Size(max = 9, message = "El teléfono debe tener entre 7 y 15 caracteres")
+    String telefono,
     @NotBlank String contrasena,
     @NotBlank String rol
 ) {
