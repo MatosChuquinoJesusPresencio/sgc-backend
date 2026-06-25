289c4abc61017dd0e6e80da6438e1799a707d9b5
AndreTs8 <andre.ts8@hotmail.com>
2026-06-25 13:14:56 -0500

    fix error de anotacion en AsignarCondominioRequest

--- src/main/java/com/condominios/sgc/infrastructure/adapter/in/web/dto/request/AsignarCondominioRequest.java
+++ src/main/java/com/condominios/sgc/infrastructure/adapter/in/web/dto/request/AsignarCondominioRequest.java
@@ -1,6 +1,8 @@
 package com.condominios.sgc.infrastructure.adapter.in.web.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 public record AsignarCondominioRequest(
-    Long idCondominio
+        @NotNull Long idCondominio
 ) {
 }
