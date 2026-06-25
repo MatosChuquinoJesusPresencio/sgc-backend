e8204d7570d3bdfcfacaf2dc0c4e4c27da7c11ed
AndreTs8 <andre.ts8@hotmail.com>
2026-06-25 18:18:34 -0500

    creando LogCombinadoProjection

--- src/main/java/com/condominios/sgc/infrastructure/adapter/out/persistence/repository/LogCombinadoProjection.java
+++ src/main/java/com/condominios/sgc/infrastructure/adapter/out/persistence/repository/LogCombinadoProjection.java
@@ -1,4 +1,10 @@
 package com.condominios.sgc.infrastructure.adapter.out.persistence.repository;
 
+import java.time.Instant;
+
 public interface LogCombinadoProjection {
+    String getTipoLog();
+    String getIdentificador();
+    String getUsuario();
+    Instant getFecha();
 }
