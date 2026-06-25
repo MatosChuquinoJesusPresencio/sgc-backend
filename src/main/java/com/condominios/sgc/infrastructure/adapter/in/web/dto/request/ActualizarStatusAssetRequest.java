88ec860479f84cdb5474415991d684a66845129a
AndreTs8 <andre.ts8@hotmail.com>
2026-06-25 13:14:25 -0500

    fix error de anotacion en CrearAssetRequest

--- src/main/java/com/condominios/sgc/infrastructure/adapter/in/web/dto/request/ActualizarStatusAssetRequest.java
+++ src/main/java/com/condominios/sgc/infrastructure/adapter/in/web/dto/request/ActualizarStatusAssetRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 
 public record ActualizarStatusAssetRequest(
     @NotBlank String tipo,
-    String estado,
+    @NotBlank String estado,
     Boolean disponible,
     @NotBlank String tipoVehiculo,
     Integer capacidadMaxima
