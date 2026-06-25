468eb31f8a8d1c74761d3eae894146efa76872bd
AndreTs8 <andre.ts8@hotmail.com>
2026-06-25 18:24:42 -0500

    metodo buscarHistorialCombinadoPaginado en LogAccesoVehicularRepositoryAdapter

--- src/main/java/com/condominios/sgc/infrastructure/adapter/out/persistence/LogAccesoVehicularRepositoryAdapter.java
+++ src/main/java/com/condominios/sgc/infrastructure/adapter/out/persistence/LogAccesoVehicularRepositoryAdapter.java
@@ -6,6 +6,8 @@ import java.time.ZoneOffset;
 import java.util.List;
 import java.util.Optional;
 
+import com.condominios.sgc.application.dto.result.AdminLogEntryResult;
+import com.condominios.sgc.infrastructure.adapter.out.persistence.repository.LogCombinadoProjection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -55,5 +57,23 @@ public class LogAccesoVehicularRepositoryAdapter implements LogAccesoVehicularRe
                 .stream()
                 .map(LogAccesoVehicularMapper::toModel)
                 .toList();
+    }
+    @Override
+    public Page<AdminLogEntryResult> buscarHistorialCombinado(Long condominioId, int pagina, int tamano) {
+        // Creamos la petición de paginación
+        PageRequest pageRequest = PageRequest.of(pagina, tamano);
+
+        // Llamamos al Native Query
+        Page<LogCombinadoProjection> proyeccionPage = repository.buscarHistorialCombinadoPaginado(condominioId, pageRequest);
+
+        // Mapeamos el resultado SQL a tu DTO (AdminLogEntryResult)
+        return proyeccionPage.map(proyeccion ->
+                new AdminLogEntryResult(
+                        proyeccion.getTipoLog(),
+                        proyeccion.getIdentificador(),
+                        proyeccion.getUsuario(),
+                        proyeccion.getFecha() // *Nota: Si tu DTO pide la fecha en String, pon proyeccion.getFecha().toString()
+                )
+        );
     }
 }
