bcf2127e2a4717858382de6041a217daeee96146
AndreTs8 <andre.ts8@hotmail.com>
2026-06-25 13:12:49 -0500

    fix error de anotacion en CrearAssetRequest

--- src/main/java/com/condominios/sgc/infrastructure/adapter/in/web/dto/request/CrearAssetRequest.java
+++ src/main/java/com/condominios/sgc/infrastructure/adapter/in/web/dto/request/CrearAssetRequest.java
@@ -1,6 +1,7 @@
 package com.condominios.sgc.infrastructure.adapter.in.web.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CrearAssetRequest(
     @NotBlank String tipo,
