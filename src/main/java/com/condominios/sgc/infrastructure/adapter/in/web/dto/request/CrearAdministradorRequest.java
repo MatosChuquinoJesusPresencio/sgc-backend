a9c4fc33c0996d5a14eb9566749181382e024115
AndreTs8 <andre.ts8@hotmail.com>
2026-06-25 12:50:18 -0500

    fix error de anotaciones fix error de anotacion en ActualizarAdministradorRequest

--- src/main/java/com/condominios/sgc/infrastructure/adapter/in/web/dto/request/CrearAdministradorRequest.java
+++ src/main/java/com/condominios/sgc/infrastructure/adapter/in/web/dto/request/CrearAdministradorRequest.java
@@ -2,12 +2,17 @@ package com.condominios.sgc.infrastructure.adapter.in.web.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record CrearAdministradorRequest(
     @NotBlank String nombres,
     @NotBlank String apellidos,
     @NotBlank @Email String correo,
+
+    @NotBlank
+    @Size(max = 9, message = "El teléfono debe tener 9 caracteres")
     String telefono,
+
     @NotBlank String contrasena
 ) {
 }
