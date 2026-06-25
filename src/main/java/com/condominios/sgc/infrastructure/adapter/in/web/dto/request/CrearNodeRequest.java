e8101c727531f3a4947472e1cdce37b4405beafa
AndreTs8 <andre.ts8@hotmail.com>
2026-06-25 12:58:52 -0500

    fix error de anotaciones fix error de anotacion en CrearNodeRequest

--- src/main/java/com/condominios/sgc/infrastructure/adapter/in/web/dto/request/CrearNodeRequest.java
+++ src/main/java/com/condominios/sgc/infrastructure/adapter/in/web/dto/request/CrearNodeRequest.java
@@ -3,14 +3,15 @@ package com.condominios.sgc.infrastructure.adapter.in.web.dto.request;
 import java.math.BigDecimal;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CrearNodeRequest(
     @NotBlank String tipo,
     @NotBlank String nombre,
     @NotBlank String nombreTorre,
-    @NotBlank Integer numero,
-    @NotBlank Integer numeroPiso,
-    @NotBlank Integer numeroApartamento,
+    @NotNull Integer numero,
+    @NotNull Integer numeroPiso,
+    @NotNull Integer numeroApartamento,
     BigDecimal metraje
 ) {
 }
